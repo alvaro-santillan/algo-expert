538dfe66c92a55004d4475bc4e799703b26a2cdd
Alvaro Santillan <afsm1995@live.com>
2020-09-01 22:10:12 -0600

    Days Progress.

--- Whiteboard/Whiteboard/main.swift
+++ Whiteboard/Whiteboard/main.swift
@@ -8,28 +8,49 @@
 
 import Foundation
 
-class Program {
-    func moveElementToEnd(_ array: inout [Int], _ toMove: Int) -> [Int] {
-        var leftPointer = 0
-        var rightPointer = (array.count-1)
-
-        while leftPointer <= rightPointer {
-            if array[leftPointer] == toMove && array[rightPointer] == toMove {
-                rightPointer -= 1
-            } else if array[leftPointer] == toMove && array[rightPointer] != toMove {
-                let tempLeftValue = array[leftPointer]
-                array[leftPointer] = array[rightPointer]
-                array[rightPointer] = tempLeftValue
-            } else if array[leftPointer] != toMove && array[rightPointer] == toMove {
-                leftPointer += 1
-                rightPointer -= 1
-            } else {
-                leftPointer += 1
+//class Program {
+    func spiralTraverse(array: [[Int]]) -> [Int] {
+        var answer = [Int]()
+        var visitedXAxis = [Int]()
+        var visitedYAxis = [Int]()
+        var currentX = 0
+        var currentY = 0
+        
+        while answer.count != (array[0].count * array.count) {
+            while !(visitedYAxis.contains(currentY)) && !(visitedXAxis.contains(currentX)) {
+                if currentY == array[0].count-1 {
+                    break
+                }
+                print(array[currentX][currentY])
+                visitedYAxis.append(currentX)
+                currentY += 1
             }
+            
+            while !(visitedYAxis.contains(currentY)) && !(visitedXAxis.contains(currentX)) {
+                if currentX == array.count-1 {
+                    break
+                }
+                print(array[currentX][currentY])
+                visitedXAxis.append(currentY)
+                currentX += 1
+            }
+            
+            
+            while !(visitedYAxis.contains(currentY)) && !(visitedXAxis.contains(currentX)) {
+                if currentX == 0 {
+                    break
+                }
+                print(array[currentX][currentY])
+//                visitedXAxis.append(currentX)
+                currentX -= 1
+            }
+            
+            return []
         }
-        return array
+        
+        return []
     }
-}
+//}
 
-var array = [1, 2, 4, 5, 3]
-print(moveElementToEnd(&array, 3))
+var array = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
+print(spiralTraverse(array: array))
