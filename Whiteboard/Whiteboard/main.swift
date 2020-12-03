57f21fb0a655810bf6f43dc689c169133d0bd1c2
Alvaro Santillan <afsm1995@live.com>
2020-12-02 21:30:56 -0700

    Days Work Rough Solution To Binary Tree Diameter Achived.

--- Whiteboard/Whiteboard/main.swift
+++ Whiteboard/Whiteboard/main.swift
@@ -8,126 +8,62 @@
 
 import Foundation
 
-//
-//  main.swift
-//  Whiteboard
-//
-//  Created by Álvaro Santillan on 8/30/20.
-//  Copyright © 2020 Álvaro Santillan. All rights reserved.
-//
-
-import Foundation
-
-var traversal = String()
-
-class Node {
-   var data: Int
-   var left: Node?
-   var right: Node?
-   init(data: Int) {
-      self.data = data
-   }
-}
-
-class BinaryTreeTraversal {
-    
-    init(choice: String, start: Node?) {
-        choiceManager(choice: choice, start: start)
-    }
-    
-    func choiceManager(choice: String, start: Node?) {
-        if choice == "Preorder" {
-            print(preOrderTraversal(node: start))
-        } else if choice == "Inorder" {
-            print(inOrderTraversal(node: start))
+class Program {
+    class BinaryTree {
+        var value: Int
+        var right: BinaryTree?
+        var left: BinaryTree?
+        var leftEdgeCount: Int?
+        var rightEdgeCount: Int?
+        
+        init(val: Int) {
+            value = val
         }
     }
     
-    func preOrderTraversal(node: Node?) {
-        guard let node = node else { return }
-        traversal.append(String(node.data) + ",")
-        preOrderTraversal(node: node.left)
-        preOrderTraversal(node: node.right)
-    }
-    func inOrderTraversal(node: Node?) {
-        if node?.data != nil {
-            inOrderTraversal(node: node!.left)
-            traversal.append(String(node!.data) + ",")
-            inOrderTraversal(node: node!.right)
+    var maxx = -1
+    func binaryTreeDiameter(tree: BinaryTree?, leftCount: Int, rightCount: Int) -> (Int,Int) {
+        // Write your code here.
+        if tree != nil {
+            var tempLeftCount = leftCount
+            var tempRightCount = rightCount
+            
+            let LeftReport = binaryTreeDiameter(tree: tree?.left, leftCount: tempLeftCount, rightCount: tempRightCount)
+            let RightReport = binaryTreeDiameter(tree: tree?.right, leftCount: tempLeftCount, rightCount: tempRightCount)
+            tempLeftCount = max(LeftReport.0, LeftReport.1) + tempLeftCount
+            tempRightCount = max(RightReport.0, RightReport.1) + tempRightCount
+//            tree?.leftEdgeCount = tempLeftCount
+//            tree?.rightEdgeCount = tempRightCount
+            
+                if tree?.right != nil {
+                    tempRightCount += 1
+                }
+                
+                if tree?.left != nil {
+                    tempLeftCount += 1
+                }
+                
+            print(tree?.value, tempLeftCount, tempRightCount)
+            if tempLeftCount + tempRightCount > maxx {
+                maxx = (tempLeftCount + tempRightCount)
+            }
+            print(maxx)
+            return(tempLeftCount, tempRightCount)
         }
-    }
-    func postOrderTraversal(node: Node?) {
-        guard let node = node else { return }
-        postOrderTraversal(node: node.left)
-        postOrderTraversal(node: node.right)
-        traversal.append(String(node.data) + ",")
+//        print(maxx)
+        return (0,0)
     }
 }
+        
+var biniary = Program()
+var root = Program.BinaryTree.init(val: 1)
+root.left = Program.BinaryTree.init(val: 3)
+root.right = Program.BinaryTree.init(val: 2)
+root.left?.left = Program.BinaryTree.init(val: 7)
+root.left?.right = Program.BinaryTree.init(val: 4)
+root.left?.left?.left = Program.BinaryTree.init(val: 8)
+root.left?.left?.left?.left = Program.BinaryTree.init(val: 9)
+//root.left?.right?.right = Program.BinaryTree.init(val: 5)
+//root.left?.right?.right?.right = Program.BinaryTree.init(val: 6)
+biniary.binaryTreeDiameter(tree: root, leftCount: 0, rightCount: 0)
 
-var root = Node(data: 1)
-root.left = Node(data: 2)
-root.right = Node(data: 3)
-root.left?.left = Node(data: 4)
-root.left?.right = Node(data: 5)
-root.right?.left = Node(data: 6)
-root.right?.right = Node(data: 7)
-BinaryTreeTraversal(choice: "Inorder", start: root)
-print(traversal)
-
-//var traversssssal = String()
-//
-//class Node {
-//    var value: Int
-//    var rightNode: Node?
-//    var leftNode: Node?
-//
-//    init(value: Int) {
-//        self.value = value
-//    }
-//}
-//
-//class BinaryTreeTraversal {
-//    init(choice: String, start: Node) {
-//        choiceManager(choice: choice, start: start)
-//    }
-//
-//    func choiceManager(choice: String, start: Node) {
-//        if choice == "Preorder" {
-//            print(preOrderTraversal(start: start, traversal: String()))
-//        } else if choice == "Inorder" {
-//            print(inOrderTraversal(start: start, traversal: String()))
-//        }
-//    }
-//
-//    func preOrderTraversal(start: Node, traversal: String) -> String {
-//
-//        if start.value != nil {
-//            traversssssal.append(String(start.value) + "-")
-//            preOrderTraversal(start: start.leftNode!, traversal: traversssssal)
-////        }
-////        if start.rightNode != nil {
-//            preOrderTraversal(start: start.rightNode!, traversal: traversssssal)
-//        }
-//
-//        return traversssssal
-//    }
-//
-//    func inOrderTraversal(start: Node, traversal: String) -> String {
-////        traversssssal.append(String(start.value) + "-")
-//
-////        if startValue = start.value
-////        inOrderTraversal(start: start.left, traversal: traversssssal)
-//
-//        return traversssssal
-//    }
-//}
-//
-//var tree = Node(value: 1)
-//tree.leftNode = Node(value: 2)
-//tree.rightNode = Node(value: 3)
-//tree.leftNode?.leftNode = Node(value: 4)
-//tree.leftNode?.rightNode = Node(value: 5)
-//tree.rightNode?.leftNode = Node(value: 6)
-//tree.rightNode?.rightNode = Node(value: 7)
-//BinaryTreeTraversal(choice: "Preorder", start: tree)
-////BinaryTreeTraversal(choice: "Inorder", start: tree)
